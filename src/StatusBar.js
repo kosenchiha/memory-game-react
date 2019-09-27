4eb3f9f93b302f17524148342df4ae5aa4e0bdfe
Vicky <kosenchiha@gmail.com>
2019-09-27 23:20:39 +0100

    Add responsive for smartphones

--- src/StatusBar.js
+++ src/StatusBar.js
@@ -5,8 +5,8 @@ const StatusBar = props => {
   const { counter, onRestart } = props;
   return (
     <div className="status-bar-container">
-      <p className="status-bar-element">{counter} moves</p>
-      <p className="status-bar-element" onClick={onRestart}>
+      <p className="status-bar-element first">{counter} moves</p>
+      <p className="status-bar-element second" onClick={onRestart}>
         Restart
       </p>
     </div>
