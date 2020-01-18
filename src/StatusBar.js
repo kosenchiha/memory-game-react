d900850d700552ac7641cc791cb00e7db38e1e48
Vicky <kosenchiha@gmail.com>
2020-01-18 22:26:07 +0000

    Add timer to calculate time of the game

--- src/StatusBar.js
+++ src/StatusBar.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
 
+const getSeconds = seconds => {
+  return ("0" + (seconds % 60)).slice(-2);
+};
+const getMinutes = seconds => {
+  return Math.floor(seconds / 60);
+};
+
 const StatusBar = props => {
-  const { counter, onRestart } = props;
+  const { counter, onRestart, time } = props;
   return (
     <div className="status-bar-container">
+      <p className="status-bar-element first">
+        {getMinutes(time)}:{getSeconds(time)}
+      </p>
       <p className="status-bar-element first">{counter} moves</p>
       <p className="status-bar-element second" onClick={onRestart}>
         Restart
